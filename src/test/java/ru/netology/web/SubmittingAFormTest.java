7a8eea46ea7e8e0585b9ce035f73dacf1fd11dcf
Artemeva D <dashartemeva7@gmail.com>
2023-09-17 21:32:16 +0500

    decision commit

---
+++ src/test/java/ru/netology/web/SubmittingAFormTest.java
@@ -0,0 +1,22 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class SubmittingAFormTest {
+    @Test
+    void shouldTest() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[action]");
+        form.$("[data-test-id=name]input").setValue("Мария-Антуанетта Иванова");
+        form.$("[data-test-id=phone]input").setValue("+784596412548");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[role=button]").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
